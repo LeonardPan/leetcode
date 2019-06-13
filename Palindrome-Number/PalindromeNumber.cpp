1ad9675e8dc61a5e7a09c37fd9010eaa8dd8cb0b
LeonardPan <panyong.zju@qq.com>
2019-06-13 10:53:04 +0800

    A simple solution

--- Palindrome-Number/PalindromeNumber.cpp
+++ Palindrome-Number/PalindromeNumber.cpp
@@ -1,68 +1,33 @@
-//palindrome number
-//Author: Leonard Pan
-
-#include <iostream>
-#include <cmath>
-using namespace std;
-
+#include <deque>
 class Solution {
 public:
-    int getPower(int number)
-    {
-        int power = 0;
-    	if(number/100000000){
-    		number/=100000000;
-    		power += 8;
-    	}
-    	if(number/10000){
-    		number/=10000;
-    		power += 4;
-    	}
-    	if(number/100){
-    		number/=100;
-    		power += 2;
-    	}
-    	if(number/10){
-    		number/=10;
-    		power+=1;
-    	}
-    	return power;
-    }
-    int getTopNthDigit(int number, int power, int place)
-    {
-    	if(place > power)
-    		return -1;
-    	return int((number / pow(10, double(power - place)))) % 10;
-    }
     bool isPalindrome(int x) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(x<0)
+        //all negtive numbers are not palindromes
+        if (x < 0) {
             return false;
-        int lMaxPower = getPower(x);
-    	int lCurPos, lHighDigit, lLowDigit;
-    	for(lCurPos = 0; lCurPos < (lMaxPower+1)/2; ++lCurPos)
-    	{
-    		lHighDigit = getTopNthDigit(x, lMaxPower, lCurPos);
-    		lLowDigit = int((x/pow(10, double(lCurPos))))%10;
-    		if(lHighDigit != lLowDigit || -1 == lHighDigit)
-    			return false;
-    	}
-    	return true;
+        }
+        //all unit numbers are palindromes
+        else if (x < 10) {
+            return true;
+        }
+        //split all digitals into a queue
+        std::deque<int> q;
+        while(x != 0) {
+            q.push_back(x % 10);
+            x /= 10;
+        }
+        //compare 2 digitals in each loop
+        int count = q.size();
+        while(count > 1) {
+            //left != right, it's not a palindrome number
+            if(q.front() != q.back()) {
+                return false;
+            }
+            q.pop_front();
+            q.pop_back();
+            count -= 2;
+        }
+        //only one or zero digital left, it's a palindrome number
+        return true;
     }
 };
-
-int main()
-{
-	Solution* lSo = new Solution();
-	int lInputNumber;
-	while(true)
-	{
-		cin >> lInputNumber;
-		if(-1 == lInputNumber)
-			break;
-		else
-			cout << (lSo->isPalindrome(lInputNumber) ? "true":"false") << endl;
-	}
-	return 0;
-}
