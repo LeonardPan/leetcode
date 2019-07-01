71990fe9d1b0ee479d22c717c698335ef43dbdf5
LeonardPan <panyong.zju@qq.com>
2019-07-01 21:14:07 +0800

    A simple dynamic programming solution.
    Possibly there's a pruning improve.

--- Longest-Palindrome-Substring/LongestPalindromeSubstring.cpp
+++ Longest-Palindrome-Substring/LongestPalindromeSubstring.cpp
@@ -1,60 +1,41 @@
+const unsigned int MAX_LENGTH = 1001;
+
+bool palindromeTable[MAX_LENGTH][MAX_LENGTH];
+
 class Solution {
 public:
+    void init(bool(&input)[MAX_LENGTH][MAX_LENGTH]) {
+        memset(&input, 0, MAX_LENGTH * MAX_LENGTH);
+    }
     string longestPalindrome(string s) {
-        int length = s.size();
-        string longestStr;
-        for (size_t i = 0; i < length; ++i)
-        {
-            if ((i + 1) * 2 < longestStr.size()) {
-                break;
-            }
-            //treat [i] as the center of the paradrome
-            string currentStr = string(1, s[i]);
-            if (longestStr.size() < 1) {
-                longestStr = currentStr;
-            }
-            for (int left = i - 1, right = i + 1;
-                left >= 0 && right < length;
-                --left, ++right)
-            {
-                //valid paradrome string
-                if (s[left] == s[right])
-                {
-                    currentStr = s[left] + currentStr + s[right];
-                    if (currentStr.size() > longestStr.size()) {
-                        longestStr = currentStr;
+        init(palindromeTable);
+        unsigned int maxLength = 1;
+        unsigned int startIndex = 0;
+        unsigned int length = s.size();
+        //for each length of substring
+        for (int len = 1; len <= length; ++len) {
+            for (int i = 0; i < length; ++i) {
+                if (len == 1) {
+                    //every single char is a valid palindrome
+                    palindromeTable[i][i] = true;
+                }
+                else if (len == 2 && i + 1 < length) {
+                    palindromeTable[i][i + 1] = (s[i] == s[i + 1]);
+                    if (palindromeTable[i][i + 1] && 2 > maxLength) {
+                        maxLength = 2;
+                        startIndex = i;
                     }
                 }
-                //invalid paradrome string
-                else {
-                    break;
-                }
-            }
-            if (i < length - 1 && s[i] == s[i + 1]) {
-                //treat [i, i+1] as the center of the paradrome
-                currentStr = string(1, s[i]) + s[i + 1];
-                if (longestStr.size() < 2) {
-                    longestStr = currentStr;
-                }
-                for (int left = i - 1, right = i + 2;
-                    left >= 0 && right < length;
-                    --left, ++right)
-                {
-                    //valid paradrome string
-                    if (s[left] == s[right])
-                    {
-                        currentStr = s[left] + currentStr + s[right];
-                        if (currentStr.size() > longestStr.size()) {
-                            longestStr = currentStr;
-                        }
-                    }
-                    //invalid paradrome string
-                    else {
-                        break;
+                else if (i + len - 1 < length) {
+                    //whether s[i, i + len - 1] is a palindrome substring? It depends on s[i + 1, i + len - 2] and itself
+                    palindromeTable[i][i + len - 1] = (palindromeTable[i + 1][i + len - 2] && s[i] == s[i + len - 1]);
+                    if (palindromeTable[i][i + len - 1] && len > maxLength) {
+                        maxLength = len;
+                        startIndex = i;
                     }
                 }
             }
         }
-        return longestStr;
+        return s.substr(startIndex, maxLength);
     }
 };
